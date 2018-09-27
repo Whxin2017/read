71bf597bd498f6d5cf61472babe3922f6f31696d
Whxin2017 <thmin1996@163.com>
2018-09-27 10:32:05 +0800

    add c

---
+++ main.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: main.c
+    > Author: WangHaoxin
+    > Mail: whxin1994@gmail.com 
+    > Created Time: 四  9/27 10:31:27 2018
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    return 0;
+}
